4f3ba2913bfc9aa812272530fcee04722cd2be08
rightone0130 <rightone0130@gmail.com>
2023-03-20 14:12:48 +0100

    ventured into javascript

---
+++ README.md
@@ -0,0 +1,1 @@
+My venture into Javascript
---
+++ js-firstTask/firstJStask.js
@@ -0,0 +1,9 @@
+let firstName = "Richard";
+let lastname = "Ayilara";
+let middleName = "Adeniyi";
+let age = "20";
+
+console.log("My name is "+ lastname, firstName, middleName +".");
+console.log("I am "+ age +" years old");
+console.log("Few of my friends call me "+ lastname +" but I like to be addressed as "+ firstName +".");
+console.log("Apart from being called "+ firstName +"," + " I also prefer to be referred to as Right-One.");
